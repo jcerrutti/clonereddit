ce4cafacbbb31ba0e9efe66293266f9e4f8f3567
Juan Cerrutti <jicerrutti@gmail.com>
2020-05-02 20:24:13 -0300

    add new action ,select post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const REQUEST_POSTS = 'REQUEST_POSTS';
 export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const POST_SELECTED = 'POST_SELECTED';
 
 function requestPosts(subreddit) {
   return {
@@ -16,11 +17,18 @@ function receivePosts(json) {
   };
 }
 
-export function fetchPosts(subreddit = 'nodejs') {
+export function fetchPosts(subreddit = 'node') {
   return (dispatch) => {
     dispatch(requestPosts(subreddit));
     return fetch(`https://www.reddit.com/r/${subreddit}.json`)
       .then((response) => response.json())
       .then((json) => dispatch(receivePosts(json)));
+  };
+}
+
+export function selectPost(post) {
+  return {
+    type: POST_SELECTED,
+    postSelected: post,
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,27 @@
-import { REQUEST_POSTS, RECEIVE_POSTS } from '../actions';
+import { REQUEST_POSTS, RECEIVE_POSTS, POST_SELECTED } from '../actions';
 
-function posts(
-  state = {
-    isFetching: false,
-    items: [],
-  },
-  action
-) {
+function topPosts(state = { isFetching: false, items: [], postSelected: null }, action) {
   switch (action.type) {
-    case REQUEST_POSTS:
-      return Object.assign({}, state, {
-        isFetching: true,
-      });
     case RECEIVE_POSTS:
       return Object.assign({}, state, {
         isFetching: false,
         items: action.posts,
+        postSelected: state.postSelected || action.posts[0],
         lastUpdated: action.receivedAt,
       });
-    default:
-      return state;
-  }
-}
-
-function topPosts(state = {}, action) {
-  switch (action.type) {
-    case RECEIVE_POSTS:
     case REQUEST_POSTS:
-      return Object.assign({}, state, posts(state[action.subreddit], action));
+      return Object.assign(
+        {},
+        state,
+        {
+          isFetching: true,
+        },
+        action
+      );
+    case POST_SELECTED:
+      return Object.assign({}, state, {
+        postSelected: action.postSelected,
+      });
     default:
       return state;
   }
