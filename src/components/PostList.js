75ce54f064df08ff9311182603545d1d950ef632
Juan Cerrutti <jicerrutti@gmail.com>
2020-05-02 21:26:41 -0300

    correctly sets date created label

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,12 +3,13 @@ import './style.css';
 import moment from 'moment';
 
 export default function PostList({ post, onClickHandler, isSelected }) {
+  const dateLabel = moment.unix(post.created_utc).utc().fromNow();
   const className = `post-list${isSelected ? ' selected' : ''}`;
   return (
     <div className={className} onClick={() => onClickHandler(post)}>
       <h6 className="title">{post.title}</h6>
       <span className="author">by {post.author}</span>
-      <span>{moment(post.created).utc().format('YYYY-MM-DD HH:mm:ss Z')}</span>
+      <span>{dateLabel}</span>
     </div>
   );
 }
