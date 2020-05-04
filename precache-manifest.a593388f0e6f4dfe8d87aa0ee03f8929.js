349a50ee9d1f04e5bd14627c38b7401e46599c46
Juan Cerrutti <jicerrutti@gmail.com>
2020-05-03 21:09:39 -0300

    Updates

---
+++ precache-manifest.a593388f0e6f4dfe8d87aa0ee03f8929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3c08b91b2d940db6dce71a66c7461e",
+    "url": "/clonereddit/index.html"
+  },
+  {
+    "revision": "af2945176e245830762b",
+    "url": "/clonereddit/static/css/main.c5af226b.chunk.css"
+  },
+  {
+    "revision": "04538ff237ac90a1072e",
+    "url": "/clonereddit/static/js/2.195361ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clonereddit/static/js/2.195361ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2945176e245830762b",
+    "url": "/clonereddit/static/js/main.8219f730.chunk.js"
+  },
+  {
+    "revision": "d70cc89b22f18ed2d0ac",
+    "url": "/clonereddit/static/js/runtime-main.263e7712.js"
+  },
+  {
+    "revision": "4c471c209d7e0fd4e83120c9f84f0079",
+    "url": "/clonereddit/static/media/comment.4c471c20.svg"
+  }
+]);
